73bc2725578aa3e1cea64a3516657f24bab0f1a6
Rodrigo Bestard Pino <49445152+rbestardpino@users.noreply.github.com>
2023-03-07 19:28:27 -0300

    void promise

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,7 +3,7 @@ import { signIn } from "next-auth/react";
 const login = () => {
   return (
     <div className="h-max w-max">
-      <button className="btn m-10" onClick={() => signIn()}>
+      <button className="btn m-10" onClick={() => void signIn()}>
         Login
       </button>
     </div>
